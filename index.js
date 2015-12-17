c7c1546e00fd97c04640c98b3290c548e8ee2267
Chris Goodchild <chgoodch@microsoft.com>
2015-12-17 17:29:21 +0000

    reformatted some of the code so that options now work correctly and are managed in a separate module

--- index.js
+++ index.js
@@ -2,37 +2,9 @@
 
 'use strict'
 
-const data = require('./lib/data')
-const parser = require('./lib/parser')
-const outputTypes = require('./lib/outputTypes')
-const commandLineArgs = require('command-line-args')
+const options = require('./lib/options').get()
+const results = require('./lib/parser').parse(require('./lib/data').get(options.src), options)
 
-const options = [
-  {name: 'help', alias: 'h', type: Boolean, description: 'Display this usage guide.'},
-  {name: 'src', alias: 's', type: String, description: 'The HAR JSON input file to process.'},
-  {name: 'options', alias: 'o', type: String, description: 'The options JSON file to include/exclude domains.'},
-  {name: 'extensions', alias: 'e', type: Boolean, description: 'Will output stats for individual file extensions.'},
-  {name: 'include', type: String, multiple: true, description: 'One or more space-separated domains/paths to include exclusively'},
-  {name: 'ignore', type: String, multiple: true, description: 'One or more space-separated domains/paths to ignore'}
-]
-
-const cli = commandLineArgs(options)
-
-try {
-  let userOptions = cli.parse()
-
-  if (userOptions.help) {
-    showHelp()
-  } else {
-    let results = parser.parse(data.get(userOptions.filename), userOptions)
-    outputTypes.console.createOutput(results, userOptions)
-    outputTypes.json.createOutput(results, userOptions)
-  }
-} catch (e) {
-  console.error('\nThe option you specified is not supported!')
-  showHelp()
-}
-
-function showHelp () {
-  console.log(cli.getUsage(options))
-}
+require('./lib/outputTypes').forEach((type) => {
+  type.createOutput(results, options)
+})
